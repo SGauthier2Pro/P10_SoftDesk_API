cf4f3a06f1bd21198c09e0a3e536a5ae6a8f6e11
SGauthier2Pro <Sylvain.Gauthier2Pro@gmail.com>
2022-11-07 22:28:29 +0100

    ajout test des droits sur les get_queryset comments et correction des fautes de retour

--- projects/views/commentviewset.py
+++ projects/views/commentviewset.py
@@ -11,12 +11,11 @@ Comment viewset class
 @version : 1.0
 """
 
-
 from rest_framework.viewsets import ModelViewSet
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
-from django.http import Http404
+from rest_framework.exceptions import ValidationError, PermissionDenied
 
 from projects.permissions import IsAuthenticated
 
@@ -39,86 +38,140 @@ class CommentViewset(MultipleSerializerMixin, ModelViewSet):
     def get_queryset(self):
         project_id = self.kwargs['project_id']
         issue_id = self.kwargs['issue_id']
-        issue = get_object_or_404(Issue.objects.filter(id=issue_id))
-        if issue.project_id.id == int(project_id):
-            queryset = Comment.objects.filter(issue_id=issue_id)
-            return queryset
+        queryset = Comment.objects.filter(issue_id=issue_id)
+        if Contributor.objects.filter(project_id=project_id,
+                                      user_id=self.request.user).exists():
+            if Issue.objects.filter(id=issue_id).exists():
+                issue = Issue.objects.get(id=issue_id)
+                if issue.project_id.id == int(project_id):
+                    return queryset
+                else:
+                    raise ValidationError(detail="This issue doesn't exists "
+                                                 "for this project")
+            else:
+                raise ValidationError(detail="This issue doesn't exists")
         else:
-            raise Http404("Ce problème n'existe pas pour ce projet")
+            raise PermissionDenied(detail="You are not contributor "
+                                          "for this project")
 
     def create(self, request, *args, **kwargs):
         project_id = self.kwargs['project_id']
-        project = get_object_or_404(
-            Project.objects.filter(id=project_id)
-        )
         issue_id = self.kwargs['issue_id']
-        issue = get_object_or_404(
-            Issue.objects.filter(id=int(issue_id))
-        )
-        contributors = Contributor.objects.filter(project_id=project.id)
-        for contributor in contributors:
-            if contributor.user_id == self.request.user:
-                serializer = self.get_serializer(
-                    data={'description': request.data['description'],
-                          'author_user_id': self.request.user.id,
-                          'issue_id': issue.id,
-                          }
-                )
-                serializer.is_valid(raise_exception=True)
-                self.perform_create(serializer)
-                headers = self.get_success_headers(serializer.data)
-                return Response(serializer.data,
-                                status=status.HTTP_201_CREATED,
-                                headers=headers)
-        return Response({'message': "Vous n'êtes pas "
-                                    "contributeur de ce projet"},
-                        status=status.HTTP_403_FORBIDDEN)
+        if Project.objects.filter(id=project_id).exists():
+            project = Project.objects.get(id=project_id)
+            if Issue.objects.filter(id=issue_id).exists():
+                issue = Issue.objects.get(id=issue_id)
+                if issue.project_id == project:
+                    if Contributor.objects.filter(project_id=project.id,
+                                                  user_id=self.request.user
+                                                  ).exists():
+                        serializer = self.get_serializer(
+                            data={'description': request.data['description'],
+                                  'author_user_id': self.request.user.id,
+                                  'issue_id': issue.id,
+                                  }
+                        )
+                        serializer.is_valid(raise_exception=True)
+                        self.perform_create(serializer)
+                        headers = self.get_success_headers(serializer.data)
+                        return Response(serializer.data,
+                                        status=status.HTTP_201_CREATED,
+                                        headers=headers)
+                    else:
+                        return Response({'message': "You are not contributor "
+                                                    "for this project"},
+                                        status=status.HTTP_403_FORBIDDEN)
+                else:
+                    return Response({'message': "This issue doesn't exists "
+                                                "for this project"},
+                                    status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return Response({'message': "This issue doesn't exists"},
+                                status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'message': "This project doesn't exists"},
+                            status=status.HTTP_400_BAD_REQUEST)
 
     def update(self, request, *args, **kwargs):
+        project_id = self.kwargs['project_id']
         issue_id = self.kwargs['issue_id']
         instance = self.get_object()
         serializer = self.get_serializer(instance,
                                          data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
-        if instance.issue_id.id == int(issue_id):
-            if instance.author_user_id.id == self.request.user.id:
-
-                instance = serializer.save()
-                self.perform_update(instance)
-                headers = self.get_success_headers(serializer.validated_data)
-                return Response(serializer.data,
-                                status=status.HTTP_206_PARTIAL_CONTENT,
-                                headers=headers)
+        if Project.objects.filter(id=project_id).exists():
+            project = Project.objects.get(id=project_id)
+            if Issue.objects.filter(id=issue_id).exists():
+                issue = Issue.objects.get(id=issue_id)
+                if issue.project_id == project:
+                    if instance.issue_id.id == issue:
+                        if instance.author_user_id.id == self.request.user.id:
+                            instance = serializer.save()
+                            self.perform_update(instance)
+                            headers = self.get_success_headers(
+                                serializer.validated_data)
+                            return Response(serializer.data,
+                                            status=status.HTTP_200_OK,
+                                            headers=headers)
+                        else:
+                            return Response(
+                                {'message': "You are not the comment's "
+                                            "author"},
+                                status=status.HTTP_403_FORBIDDEN
+                            )
+                    else:
+                        return Response(
+                            {"message": "This comment doesn't exists for "
+                                        "this issue"},
+                            status=status.HTTP_400_BAD_REQUEST
+                        )
+                else:
+                    return Response(
+                        {'message': "This issue doesn't exists "
+                                    "for this project"},
+                        status=status.HTTP_400_BAD_REQUEST)
             else:
-                return Response(
-                    {'message': "Vous n'êtes pas authorisé à"
-                                " modifier ce commentaire"},
-                    status=status.HTTP_403_FORBIDDEN
-                )
+                return Response({'message': "This issue doesn't exists"},
+                                status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response(
-                {
-                    'message': "ce commentaire n'existe pas pour ce problème"},
-                status=status.HTTP_404_NOT_FOUND
-            )
+            return Response({'message': "This project doesn't exists"},
+                            status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, *args, **kwargs):
+        project_id = self.kwargs['project_id']
         issue_id = self.kwargs['issue_id']
         instance = self.get_object()
         user = request.user
-        if instance.issue_id.id == int(issue_id):
-            if instance.author_user_id == user:
-                self.perform_destroy(instance)
-                return Response(
-                    {'message': 'Le commentaire a bien été supprimer'},
-                    status=status.HTTP_200_OK)
+        if Project.objects.filter(id=project_id).exists():
+            project = Project.objects.get(id=project_id)
+            if Issue.objects.filter(id=issue_id).exists():
+                issue = Issue.objects.get(id=issue_id)
+                if issue.project_id == project:
+                    if instance.issue_id == issue:
+                        if instance.author_user_id == user:
+                            self.perform_destroy(instance)
+                            return Response(
+                                {'message': 'The comment has been deleted'},
+                                status=status.HTTP_200_OK)
+                        else:
+                            return Response({'message': "You are not "
+                                                        "authorized to delete "
+                                                        "this comment"},
+                                            status=status.HTTP_403_FORBIDDEN)
+                    else:
+                        return Response(
+                            {"message": "This comment doesn't exists for "
+                                        "this issue"},
+                            status=status.HTTP_400_BAD_REQUEST
+                        )
+                else:
+                    return Response(
+                        {'message': "This issue doesn't exists "
+                                    "for this project"},
+                        status=status.HTTP_400_BAD_REQUEST)
             else:
-                return Response({'message': "Vous n'êtes pas authorisé à"
-                                " supprimer ce commentaire"},
-                                status=status.HTTP_403_FORBIDDEN)
+                return Response({'message': "This issue doesn't exists"},
+                                status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response(
-                {
-                    'message': "ce commentaire n'existe pas pour ce problème"},
-                status=status.HTTP_404_NOT_FOUND
-            )
+            return Response({'message': "This project doesn't exists"},
+                            status=status.HTTP_400_BAD_REQUEST)
