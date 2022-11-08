db95854ddf1b003e69099b386e9c57f759eb0a6e
SGauthier2Pro <Sylvain.Gauthier2Pro@gmail.com>
2022-11-09 00:33:42 +0100

    correction des erreurs PEP8

--- SoftDesk_API/settings.py
+++ SoftDesk_API/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jw_!wy&-19h8o0+*wo3_=-e=+g51t!^c09#45u+5-sj(dkpu$t'
+SECRET_KEY = \
+    'django-insecure-jw_!wy&-19h8o0+*wo3_=-e=+g51t!^c09#45u+5-sj(dkpu$t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -91,16 +92,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+                'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+                'MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+                'CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+                'NumericPasswordValidator',
     },
 ]
 
--- projects/views/commentviewset.py
+++ projects/views/commentviewset.py
@@ -12,7 +12,6 @@ Comment viewset class
 """
 
 from rest_framework.viewsets import ModelViewSet
-from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.exceptions import ValidationError, PermissionDenied
